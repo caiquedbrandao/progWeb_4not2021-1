a7332fe3373452f3081e2aa3857d02a3f9f5c984
Caíque Brandão <caique.dantas.brandao@gmail.com>
2021-02-25 01:56:10 +0000

    First Commit

--- revisao-js/propriedades-abreviadas.js
+++ revisao-js/propriedades-abreviadas.js
@@ -9,7 +9,7 @@ let lastLogin = '2021-02-08 22:49:28'
 let userInfo = {
   login: usuario,
   password: senha,
-  name, // Propriedade abreviada (name: name)
+  name, // Propriedade abreviada (name: name), pode ser usada quando os nome coincidem propriedade = nome_variavel
   lastLogin // (lastLogin: lastLogin)
 }
 
@@ -17,4 +17,4 @@ console.log(userInfo)
 
 let info = 'Isto é um teste'
 console.log(info)
-console.log({info})
+console.log({info}) // exibe o valor da variável dentro de um objeto de propriedade/valor (dicionario)
